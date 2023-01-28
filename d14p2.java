891150085b43d101da8ceff82129e936a02bbf6b
Matheus Cunha Motta <52633+matheuscunhamotta@users.noreply.github.com>
2023-01-28 18:44:15 -0300

    Solution to day 14 part 2
    Forked from the `HashMap` implementation of part 1, since it have a
    `toString` method.

---
+++ d14p2.java
@@ -0,0 +1,200 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+
+public class d14p2 {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("input/d14.txt"));
+        Cave cave = new Cave();
+        cave.scan(lines);
+
+        int count = 0;
+        while (true) {
+            count += 1;
+            Point currentSand = cave.newSand();
+            cave.dropSand(currentSand);
+            // Test if the current sand rested at origin.
+            if (currentSand.getX() == 500 && currentSand.getY() == 0)
+                break;
+        }
+        System.out.println(cave);
+        System.out.println(count);
+    }
+}
+
+
+class Point {
+    private int x;
+    private int y;
+    private boolean isRock;
+
+    public Point(int x, int y, boolean isRock) {
+        this.x = x;
+        this.y = y;
+        this.isRock = isRock;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isRock() {
+        return isRock;
+    }
+
+    public void translate(int[] direction) {
+        x = x + direction[0];
+        y = y + direction[1];
+    }
+
+    public static Point createSand() {
+        return new Point(500, 0, false);
+    }
+
+    /** Format: x,y */
+    public static Point fromString(String point) {
+        String[] parts = point.split(",");
+        int x = Integer.parseInt(parts[0]);
+        int y = Integer.parseInt(parts[1]);
+        return new Point(x, y, true);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + getX() + "," + getY() + ")";
+    }
+}
+
+
+class Cave {
+    private HashMap<String, Point> cave = new HashMap<>();
+    private static final int[][] directions = {{0, 1}, {-1, 1}, {1, 1}};
+    private int mostLeft = 500;
+    private int mostRight = 500;
+    private int deepestRock = 0;
+
+    private void addPoint(Point point) {
+        cave.put(point.toString(), point);
+        // Dynamically update the cave boundaries.
+        updateBoundaries(point);
+    }
+
+    private void updateBoundaries(Point point) {
+        if (point.getY() > deepestRock) {
+            deepestRock = point.getY();
+        }
+        if (point.getX() < mostLeft) {
+            mostLeft = point.getX();
+        }
+        if (point.getX() > mostRight) {
+            mostRight = point.getX();
+        }
+    }
+
+    private Point getPoint(String coordinates) {
+        return cave.get(coordinates);
+    }
+
+    /** Format: (x,y) */
+    private boolean cellIsEmpty(String point) {
+        Point target = Point.fromString(point.substring(1, point.length() - 1));
+        if (target.getY() < deepestRock)
+            return !cave.containsKey(point);
+        return false;
+    }
+
+    private static String getStringFmt(int x, int y) {
+        return "(" + x + "," + y + ")";
+    }
+
+    public Point newSand() {
+        Point point = Point.createSand();
+        addPoint(point);
+        return point;
+    }
+
+    public void dropSand(Point sand) {
+        outer: while (true) {
+            for (int i = 0; i < 3; i++) {
+                int dx = sand.getX() + directions[i][0];
+                int dy = sand.getY() + directions[i][1];
+                String nextPos = getStringFmt(dx, dy);
+                if (cellIsEmpty(nextPos)) {
+                    // Remove from the list before mutation, to avoid key issues.
+                    cave.remove(sand.toString());
+                    sand.translate(directions[i]);
+                    addPoint(sand);
+                    break;
+                }
+                // If all three directions have been tested and none broke the loop, then the sand
+                // came to rest.
+                if (i == 2)
+                    break outer;
+            }
+        }
+    }
+
+    public void scan(List<String> lines) {
+        for (String line : lines) {
+            String[] points = line.split(" -> ");
+            for (int i = 0; i < points.length - 1; i++) {
+                String headString = points[i + 1];
+                String tailString = points[i];
+                Point head = Point.fromString(headString);
+                Point tail = Point.fromString(tailString);
+                makeSegment(head, tail);
+            }
+        }
+        deepestRock = deepestRock + 2;
+    }
+
+    private Point makeSegment(Point head, Point tail) {
+        addPoint(head);
+        addPoint(tail);
+        int dx = head.getX() - tail.getX();
+        int dy = head.getY() - tail.getY();
+        int taxicabDistance = Math.abs(dx) + Math.abs(dy);
+        // If points are within the taxicab unit "circle", return.
+        if (taxicabDistance <= 1)
+            return tail;
+
+        // Direction vectors.
+        int i = (int) Math.signum(dx);
+        int j = (int) Math.signum(dy);
+        Point newTail = new Point(tail.getX() + i, tail.getY() + j, true);
+        addPoint(newTail);
+        return makeSegment(head, newTail);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int j = 0; j <= deepestRock; j++) {
+            for (int i = mostLeft; i <= mostRight; i++) {
+                if (j == deepestRock) {
+                    stringBuilder.append("#");
+                    continue;
+                }
+                if (i == 500 && j == 0) {
+                    stringBuilder.append("+");
+                    continue;
+                }
+                String currentCell = getStringFmt(i, j);
+                if (!cellIsEmpty(currentCell)) {
+                    Point point = getPoint(currentCell);
+                    stringBuilder.append(point.isRock() ? "#" : "o");
+                } else {
+                    stringBuilder.append(".");
+                }
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
